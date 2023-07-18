858e20080120b4027ad506075d85d6dec97bb4ef
akshansh <akshansh456@gmail.com>
2023-07-18 10:34:00 +0530

    make migrations

--- trackip/trackip/urls.py
+++ trackip/trackip/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
+    path('signup/',views.user_signup,name='sign_up'),
+    path('login/',views.user_login,name='log_in'),
+    path('dashboard/',views.dashboard,name='dashboard'),
+    path('logout/',views.user_logout,name='log_out'),
+    path('add/',views.add_post,name='add'),
+    path('update/<int:id>/',views.update_post,name='update'),
+    path('delete/<int:id>/',views.delete_post,name='delete'),
 ]
